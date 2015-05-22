da50f69504804db391fdd1d0bf091f0fe1892604
Singleton06 <Singleton06@gmail.com>
2015-05-22 07:17:12 -0500

    updated to support pure daisy and daybloom

--- src/CraftFlower.lua
+++ src/CraftFlower.lua
@@ -5,6 +5,8 @@ os.loadAPI("Recipes")
 if table.getn(args) < 2 then
   print("Usage - CraftFlower <flower> <count>")
   print("Available Flowers:")
+  print("pureDaisy")
+  print("daybloom")
   print("thermalily")
   print("hopperhock")
   print("jaded") -- crafts jadedAmaranthus, no one wants to spell that out though
@@ -19,4 +21,8 @@ elseif name == "thermalily" then
   cInv.craftFlowers(Recipes.thermalily, num)
 elseif name == "jaded" then
   cInv.craftFlowers(Recipes.jadedAmaranthus, num)
+elseif name == "daybloom" then
+  cInv.craftFlowers(Recipes.daybloom, num)
+elseif name == "pureDaisy" then
+  cInv.craftFlowers(Recipes.pureDaisy, num)
 end
