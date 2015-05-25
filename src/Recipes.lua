a75473e62cde04b5606a109c5c3f9e8ab5f59d34
Singleton06 <Singleton06@gmail.com>
2015-05-25 07:00:16 -0500

    Added the rest of the runes recipes

--- src/Recipes.lua
+++ src/Recipes.lua
@@ -60,3 +60,85 @@ runeOfEarth =
     "Mushroom",
     "Stone",
     "Block of Coal"}
+  
+  runeOfWater =
+   {"Manasteel Ingot",
+    "Manasteel Ingot",
+    "Manasteel Ingot",
+    "Bone Meal",
+    "Fishing Rod",
+    "Sugar Cane"}
+  
+runeOfSpring = 
+ {"Rune of Water",
+  "Rune of Fire",
+  "Oak Sapling",
+  "Oak Sapling",
+  "Oak Sapling",
+  "Wheat"}
+
+runeOfSummer = 
+ {"Rune of Earth",
+  "Rune of Air",
+  "Sand",
+  "Sand",
+  "Slimeball",
+  "Melon"}
+
+runeOfAutumn = 
+ {"Rune of Air",
+  "Rune of Fire",
+  "Oak Leaves",
+  "Oak Leaves",
+  "Oak Leaves",
+  "Spider Eye"}
+
+runeOfWinter = 
+ {"Rune Of Earth",
+  "Rune of Water",
+  "Snow",
+  "Snow",
+  "Wool",
+  "Cake"}
+
+runeOfMana = {"Manasteel Ingot",
+  "Manasteel Ingot",
+  "Manasteel Ingot",
+  "Manasteel Ingot",
+  "Manasteel Ingot",
+  "Mana Pearl"}
+
+runeOfLust = {"Rune of Air",
+  "Rune of Summer",
+  "Mana Diamond",
+  "Mana Diamond"}
+
+runeOfGluttony = {"Rune of Fire",
+  "Rune of Winter",
+  "Mana Diamond",
+  "Mana Diamond"}
+
+runeOfGreed = {"Rune of Water",
+  "Rune of Spring",
+  "Mana Diamond",
+  "Mana Diamond"}
+
+runeOfSloth = {"Rune of Air",
+  "Rune of Autumn",
+  "Mana Diamond",
+  "Mana Diamond"}
+
+runeOfWrath = {"Rune of Earth",
+  "Rune of Winter",
+  "Mana Diamond",
+  "Mana Diamond"}
+
+runeOfEnvy = {"Rune of Water",
+  "Rune of Winter",
+  "Mana Diamond",
+  "Mana Diamond"}
+
+runeOfPride = {"Rune of Fire",
+  "Rune of Summer",
+  "Mana Diamond",
+  "Mana Diamond"}
