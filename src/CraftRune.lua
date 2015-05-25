4719994dd3a13d648a83485e873fee491a6b4902
Singleton06 <Singleton06@gmail.com>
2015-05-25 07:12:07 -0500

    Added ability to craft all runes

--- src/CraftRune.lua
+++ src/CraftRune.lua
@@ -8,6 +8,12 @@ if table.getn(args) < 2 then
   print("runeOfAir")
   print("runeOfEarth")
   print("runeOfFire")
+  print("runeOfWater")
+  print("runeOfSpring")
+  print("runeOfSpring")
+  print("runeOfSpring")
+  print("runeOfSpring")
+  print("runeOfSpring")
  
 end
  
@@ -20,4 +26,30 @@ elseif name == "runeOfFire" then
   cInv.craftRunes(Recipes.runeOfFire, num)
 elseif name == "runeOfAir" then
   cInv.craftRunes(Recipes.runeOfAir, num)
+elseif name == "runeOfWater" then
+  cInv.craftRunes(Recipes.runeOfWater, num)
+elseif name == "runeOfSpring" then
+  cInv.craftRunes(Recipes.runeOfSpring, num)
+elseif name == "runeOfSummer" then
+  cInv.craftRunes(Recipes.runeOfSummer, num)
+elseif name == "runeOfAutumn" then
+  cInv.craftRunes(Recipes.runeOfAutumn, num)
+elseif name == "runeOfWinter" then
+  cInv.craftRunes(Recipes.runeOfWinter, num)
+elseif name == "runeOfMana" then
+  cInv.craftRunes(Recipes.runeOfMana, num)
+elseif name == "runeOfLust" then
+  cInv.craftRunes(Recipes.runeOfLust, num)
+elseif name == "runeOfGluttony" then
+  cInv.craftRunes(Recipes.runeOfGluttony, num)
+elseif name == "runeOfGreed" then
+  cInv.craftRunes(Recipes.runeOfGreed, num)
+elseif name == "runeOfSloth" then
+  cInv.craftRunes(Recipes.runeOfSloth, num)
+elseif name == "runeOfWrath" then
+  cInv.craftRunes(Recipes.runeOfWrath, num)
+elseif name == "runeOfEnvy" then
+  cInv.craftRunes(Recipes.runeOfEnvy, num)
+elseif name == "runeOfPride" then
+  cInv.craftRunes(Recipes.runeOfPride, num)
 end
